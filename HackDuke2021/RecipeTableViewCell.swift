c4a80212f946461b39ad4423822e56c406f2238b
Luiza Wolf <ahw26@duke.edu>
2021-10-23 18:01:41 -0400

    added table view controllers and cell files

---
+++ HackDuke2021/RecipeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeTableViewCell.swift
+//  HackDuke2021
+//
+//  Created by Luiza Wolf on 10/23/21.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
